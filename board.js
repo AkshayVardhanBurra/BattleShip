dc031b569b36caab42971321031031d5620beb0e
Akshay Vardhan Burra <142847672+AkshayVardhanBurra@users.noreply.github.com>
2025-08-05 17:33:39 -0700

    Finished features 1 and 2.
    User can now place ships and computer gets all its ships generated randomly.

--- board.js
+++ board.js
@@ -4,15 +4,7 @@
 //2 -> hit
 //3 -> occupied
 
-
-export class Board{
-    constructor(){
-        this.board = this.createEmptyBoard(10, 10);
-
-    }
-
-
-    createEmptyBoard(rows, cols){
+function createEmptyBoard(rows, cols){
         let brows = [];
         for(let i = 0; i < rows; i++){
             let row = [];
@@ -25,8 +17,119 @@ export class Board{
             
         }
 
-        return rows;
+        return brows;
+    }
+export class Board{
+    constructor(){
+        this.board = createEmptyBoard(10, 10);
+
     }
 
 
+    
+
+
+    //return: void -> places the ships randomly on the board
+    placeShipsRandomly(){
+        // let rPos = getRandom();
+        // let HorV = randomHorV();
+        // while(this.hasSpace(rPos, 3, HorV)){
+        //     rPos = getRandom();
+        //     HorV = randomHorV();
+        // }
+        // this.placeShip(33, 3, rPos, HorV);
+
+        this.placeAShipRandomly(33, 3);
+
+        for(let i = 2; i <= 5; i++){
+            // rPos = getRandom();
+            // HorV = randomHorV();
+            // while(this.hasSpace(rPos, i, HorV)){
+            //     rPos = getRandom();
+            //     HorV = randomHorV();
+            // }
+
+            // this.placeShip(i, i, rPos, HorV);
+            this.placeAShipRandomly(i, i);
+        }
+    }
+
+    placeAShipRandomly(shipNum, spaces){
+        let rPos = this.getRandom();
+        let HorV = this.randomHorV();
+        while(!this.hasSpace(rPos, spaces, HorV)){
+            rPos = this.getRandom();
+            HorV = this.randomHorV();
+        }
+        this.placeShip(shipNum, spaces, rPos, HorV);
+    }
+
+    //return: void -> changes board 2d array by placing the ship
+    //position is an array
+    placeShip(boatNum,spaces, position, isHorizontal = true){
+        if(isHorizontal){
+            console.log("placing horizontally!")
+            for(let x = position[1]; x > position[1] - spaces; x--){
+                this.board[position[0]][x] = boatNum;
+            }
+        }else{
+            console.log("placing vertically!")
+            for(let y = position[0]; y < position[0] + spaces; y++){
+                this.board[y][position[1]] = boatNum;
+            }
+        }
+    }
+
+    hasSpace(position, spaces, isHorizontal = true){
+        let x = position[1];
+        let y = position[0];
+
+        if(isHorizontal){
+            if((x + 1) - spaces < 0){
+                return false;
+            }
+
+            return this.searchForShips(position, spaces, isHorizontal);
+        }else{
+            if((y) + spaces > this.board.length){
+                return false;
+            }
+
+            return this.searchForShips(position, spaces, isHorizontal);
+        }
+    }
+
+    searchForShips(position, spaces, isHorizontal=true){
+        if(isHorizontal){
+            for(let i = position[1]; i > position[1] - spaces; i--){
+                if(this.board[position[0]][i] != 0){
+                    return false;
+                }
+            }
+
+
+        }else{
+            for(let i = position[0]; i < position[0] + spaces; i++){
+                if(this.board[i][position[1]] != 0){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    getNumRandom(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
+
+    getRandom(){
+        return [this.getNumRandom(0,10), this.getNumRandom(0, 10)];
+    }
+    //true for horizontal. false for vertical
+    randomHorV(){
+        let randomNum = Math.random();
+
+        return randomNum<=0.5
+    }
 }
--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 import {Board} from "./board.js";
 
-const body = document.querySelector("body");
+const body = document.querySelector("main");
+let horizontal = true;
+const carrierBtn = document.getElementById("carrier");
+const battleshipBtn = document.getElementById("battleship");
+const cruiserBtn = document.getElementById("cruiser");
+const submarineBtn = document.getElementById("submarine");
+const destroyerBtn = document.getElementById("destroyer");
+const rotate = document.getElementById("rotate");
+let gameStarted = false;
+let totalShips = 5;
+let currentShip = 0;
+let currentButton = null;
+let currentBoatNum = -1;
 
+const highlightedDivs = [];
+
+function validConsts(){
+    return currentBoatNum > 0 && currentShip > 0 && totalShips > 0;
+}
 
 //returns the whole board as node
-function createBoard(){
+function createBoard(player=true){
     let div = document.createElement("div");
     div.style="margin:2rem;"
     for(let i = 0; i < 10; i++){
-        let s=createRow(i);
+        let s=createRow(i, player);
         div.appendChild(s);
     }
 
     return div;
 }
 
-function createRow(row){
+function createRow(row, player=true){
     let sect = document.createElement("section");
     for(let i = 0; i < 10; i++){
-        let d = createDiv("empty", row, i);
+        let d = createDiv("empty", row, i, player);
         sect.appendChild(d);
     }
 
@@ -27,7 +44,38 @@ function createRow(row){
     return sect;
 }
 
-function createDiv(status, row, col){
+function highlightDivs(list, spaces, mainDiv, isHorizontal = true){
+    
+    
+    let coords = getCoords(mainDiv.id);
+    if(isHorizontal){
+        
+        for(let i = coords[1]; i > coords[1] - spaces; i--){
+            //highlight the div;
+            let box = document.getElementById(coords[0] + " " + i);
+            box.classList.add("highlight");
+            list.push(box);
+        }
+    }else{
+        console.log("vertical highlighting!")
+        for(let i = coords[0]; i < coords[0] + spaces; i++){
+            let box = document.getElementById(i + " " + coords[1]);
+            box.classList.add("highlight");
+            list.push(box);
+        }
+    }
+}
+
+function removeHighlighting(list){
+    //go through list and remove the highlight class from classlist.
+
+    while(list.length != 0){
+        let box = list.pop();
+        box.classList.remove("highlight");
+    }
+}
+
+function createDiv(status, row, col, player=true){
 
     let box=document.createElement("div");
     box.id=row+" " + col;
@@ -37,17 +85,183 @@ function createDiv(status, row, col){
         box.classList.add(status);
 
     }
+    if(player){
+        box.onmouseover = (e) => {
+            if(horizontal && playerBoard.hasSpace(getCoords(e.target.id), currentShip)){
+                removeHighlighting(highlightedDivs);
+                highlightDivs(highlightedDivs, currentShip, e.target, horizontal);
+                
+            }else if(!horizontal && playerBoard.hasSpace(getCoords(e.target.id), currentShip, false)){
+                removeHighlighting(highlightedDivs);
+                highlightDivs(highlightedDivs, currentShip, e.target, horizontal);
+            }else{
+                removeHighlighting(highlightedDivs);
+            }
+        }
+    }
+
+    
+
+    if(player){
+        box.onclick = (e) => {
+            if(gameStarted){
+                //game logic here
+            }else{
+                handleShipsLogic(e.target);
+                
+            }
+        }
+    }else{
+        box.onclick = (e) => {
+            //handle marking and hitting.
+        }
+    }
 
     return box;
 
+}
+
+function handleShipsLogic(box){
+    console.log("here!!!!")
+    console.log(validConsts());
+    console.log(horizontal);
+    console.log(playerBoard.hasSpace(getCoords(box.id), currentShip));
+    console.log(playerBoard.board[0]);
+    if(validConsts() && horizontal && playerBoard.hasSpace(getCoords(box.id), currentShip)){
+                console.log("placing horizintally again!")
+                playerBoard.placeShip(currentBoatNum, currentShip, getCoords(box.id));
+                updateAllDivs(playerBoard, playerUIBoard);
+                console.log(playerBoard.board);
+                
+                currentButton.remove();
+                totalShips --;
+                resetConsts();
+                
+    }else if(validConsts() && playerBoard.hasSpace(getCoords(box.id), currentShip, false) && !horizontal){
+                playerBoard.placeShip(currentBoatNum, currentShip, getCoords(box.id), false);
+                updateAllDivs(playerBoard, playerUIBoard);
+               
+                currentButton.remove();
+                totalShips--;
+                resetConsts();
+    }
+}
+
+function resetConsts(){
+    currentBoatNum = -1;
+    currentShip = 0;    
+    currentButton = null;
+}
+
+function getCoords(id){
+    let coords = id.split(" ");
+
+    for(let i = 0; i < coords.length; i++){
+        coords[i] = parseInt(coords[i]);
+    }
+
+    return coords;
 }
 
 
 let playerBoard = new Board();
 let compBoard = new Board();
 
-let playerUIBoard = createBoard();
-let compUIBoard = createBoard();
+const playerUIBoard = createBoard(true);
+const compUIBoard = createBoard(false);
 
 body.appendChild(playerUIBoard);
 body.appendChild(compUIBoard);
+
+//------------------------------------------
+
+
+document.getElementById("play").onclick = (e) => {
+    if(totalShips > 0){
+        alert("Place all your ships before playing!")
+        return;
+    }else{
+        //basically enables div onclicks to work
+        gameStarted = true;
+        compBoard.placeShipsRandomly();
+        console.log(compBoard.board);
+        updateAllDivs(compBoard, compUIBoard);
+        rotate.remove();
+        e.target.remove();
+        
+    }
+
+
+}
+
+
+
+
+
+
+
+carrierBtn.onclick = (e) => {
+    console.log("clicked carrier");
+    currentShip = 5;
+    currentButton = carrierBtn;
+    currentBoatNum = 5;
+    
+};
+
+battleshipBtn.onclick = (e) => {
+    currentShip = 4;
+    currentButton = battleshipBtn;
+    currentBoatNum = 4;
+};
+
+cruiserBtn.onclick = (e) => {
+    currentShip = 3;
+    currentButton = cruiserBtn;
+    currentBoatNum = 3;
+    
+};
+
+submarineBtn.onclick = (e) => {
+    currentShip = 3;
+    currentButton = submarineBtn;
+    currentBoatNum = 33;
+    
+};
+
+destroyerBtn.onclick = () => {
+    currentShip = 2;
+    currentButton = destroyerBtn;
+    currentBoatNum = 2;
+    
+};
+
+rotate.onclick = (e) => {
+    horizontal = !horizontal;
+}
+
+
+
+
+
+function updateAllDivs(internalBoard, externalBoard){
+    let sectionChildren = externalBoard.children;
+
+    for(let i = 0; i < sectionChildren.length; i++){
+        let boxChildren = sectionChildren[i].children;
+        for(let j = 0; j < boxChildren.length; j++){
+            updateColor(boxChildren[j], internalBoard.board[i][j]);
+        }
+    }
+}
+
+function updateColor(box, num){
+    if(num == 0){
+        box.className = "box empty";
+    }else if(num == 1){
+        box.className = "box marked";
+    }else if(num <= 5 && num >= 2 || num > 15){
+        box.className = "box occupied";
+    }else if(num == 7){
+        box.className = "hit";
+    }
+}
